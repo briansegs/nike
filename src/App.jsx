884ea6f4b8e7dc0b51b3a9c82767f08e61166e2f
bsegers <briansegs@gmail.com>
2024-04-22 17:39:34 -0400

    feat: Added base components to structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality
+} from './sections';
 
 const App = () => (
     <main className='relative'>
       Nav
       <section className='xl:padding-1 wide:padding-r padding-b'>
-        Hero
+        <Hero />
       </section>
       <section className='padding'>
-        PopularProducts
+        <PopularProducts />
       </section>
       <section className='padding'>
-        SuperQuality
+        <SuperQuality />
       </section>
       <section className='padding-x py-10'>
-        Services
+        <Services />
       </section>
       <section className='padding'>
-        SpecialOffer
+        <SpecialOffer />
       </section>
       <section className='bg-pale-blue padding'>
-        CustomerReviews
+        <CustomerReviews />
       </section>
       <section className='padding-x sm:py-32 py-16 w-full'>
-        Subscribe
+        <Subscribe />
       </section>
       <section className='bg-black padding-x padding-t pb-8 padding'>
-        <p className='text-white'>Footer</p>
+        <Footer />
       </section>
     </main>
 );
