90b9fcebcd52a965bf2f8c86810a8bcb19a1c97e
bsegers <briansegs@gmail.com>
2024-04-24 09:04:36 -0400

    fix: Typo in styles

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
   const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
 
   return (
-    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-containe">
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-sart w-full max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">
           Our Summer Collection
