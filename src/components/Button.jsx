2e5795fdb71af267c2048a3572d24e4ba3da0036
bsegers <briansegs@gmail.com>
2024-04-24 16:05:22 -0400

    feat: Modify Button for alternate style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,10 @@
-
-
-const Button = ({ label, iconURL} ) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor} ) => {
   return (
     <button 
-        className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red w-fit">
+        className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full w-fit 
+        ${backgroundColor 
+          ? `${backgroundColor} ${borderColor} ${textColor}` 
+          : "bg-coral-red text-white border-coral-red"} `}>
         {label} 
         {iconURL && <img 
             src={iconURL} 
